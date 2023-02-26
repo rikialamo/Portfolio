9c51eadc8567c17c5c141c52a95927e6c8747518
rikialamo <rikialamo@gmail.com>
2023-02-26 18:16:45 +0100

    Ejercicio 1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log("NO HACER NADA AÚN")
+
+// ejercicio 1
+
+const blackButton = document.querySelector('.btn-dark');
+const blueButton = document.querySelector('.btn-primary');
+const redButton = document.querySelector('.botonRojo');
+const whiteButton = document.querySelector('.btn-light');
+
+blackButton.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+});
+
+blueButton.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'blue';
+    document.body.style.color = 'white';
+});
+
+redButton.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'red';
+    document.body.style.color = 'white';
+});
+
+whiteButton.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'white';
+    
+});
